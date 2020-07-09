151f6f60dd7e20295aef3a9a7fee0c5c6039303a
geraldos <sepdwijaya18@gmail.com>
2020-07-09 18:27:35 +0700

    edit styling and add privateroute feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { Login, Home } from "./pages";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import PrivateRoute from "./helpers/PrivateRoute";
 import { createStore } from "redux";
 import { Provider } from "react-redux";
 import reducers from "./redux/reducers";
@@ -15,9 +16,9 @@ function App() {
                     <Route exact path="/">
                         <Login />
                     </Route>
-                    <Route exact path="/home">
+                    <PrivateRoute exact path="/home">
                         <Home />
-                    </Route>
+                    </PrivateRoute>
                 </Switch>
             </Router>
         </Provider>
--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,8 +2,13 @@ import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { useHistory } from "react-router-dom";
 import { logout } from "../../redux/actions";
-
+import styled from "styled-components";
 import { Container, Row, Col, Button } from "reactstrap";
+
+const Center = styled.div`
+    display: flex;
+    justify-content: center;
+`;
 
 export default function Home() {
     const user = useSelector((state) => state);
@@ -21,17 +26,22 @@ export default function Home() {
                 {dataUser.profile !== undefined && (
                     <Col>
                         <h1 style={{ textAlign: "center" }}>Ini adalah Home</h1>
+                        <Center>
+                            <img src={dataUser.profile.imageUrl} alt="" />
+                        </Center>
 
-                        <img src={dataUser.profile.imageUrl} alt="" />
+                        <h3 style={{ textAlign: "center" }}>
+                            Name : {dataUser.profile.name}
+                        </h3>
 
-                        <h3>Name : {dataUser.profile.name}</h3>
-
-                        <h3>Email : {dataUser.profile.email}</h3>
-                        <Col>
+                        <h3 style={{ textAlign: "center" }}>
+                            Email : {dataUser.profile.email}
+                        </h3>
+                        <Center>
                             <Button onClick={() => dispatch(logout(history))}>
                                 Logout
                             </Button>
-                        </Col>
+                        </Center>
                     </Col>
                 )}
             </Row>
